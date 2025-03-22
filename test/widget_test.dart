45db682c82fac715d49edd47e18e52a64a81a48a
tanvir5386 <islam15-5386@diu.edu.bd>
2025-03-22 09:22:16 +0600

    hi

--- test/widget_test.dart
+++ test/widget_test.dart
@@ -10,10 +10,10 @@ import 'package:flutter_test/flutter_test.dart';
 
 import 'package:ecommerce_tan/main.dart';
 
-void main() {
+void main() { 
   testWidgets('Counter increments smoke test', (WidgetTester tester) async {
     // Build our app and trigger a frame.
-    await tester.pumpWidget(const FreebiesEcom());
+    await tester.pumpWidget(const FreebiesEcom()); //
 
     // Verify that our counter starts at 0.
     expect(find.text('0'), findsOneWidget);
